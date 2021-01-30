8a60c5a9b5a380d6dd44bdf30d3c51b7cfc54e48
Pradeep Sekar <sek.pradeep1@gmail.com>
2021-01-30 13:28:39 +0530

    Made some changes

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
 # #! python3
-import math
-import os
-import sys
+
 
 import requests
 
-print("Hello World!")
+# print("Hello World!")
 r = requests.get("https://docs.anaconda.com/anaconda/install/windows/")
 print(r.status_code)
+print(r.ok)
